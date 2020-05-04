dfed0fecd730333895b337052cfb87937ff33686
Roman Grubic <grubic.roman@gmail.com>
2020-05-04 15:00:26 +0000

    Recommended way of using useState().

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,9 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-    const [inputState, setInputState] = useState({ title: '', amount: '' });
+    // split your state into multiple states (manage independetly)
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('')
 
     const submitHandler = event => {
         event.preventDefault();
@@ -20,26 +22,18 @@ const IngredientForm = React.memo(props => {
                         <input
                             type="text"
                             id="title"
-                            value={ inputState.title }
+                            value={ enteredTitle }
                             onChange={ event => {
-                                const newTitle = event.target.value;
-                                setInputState((prevInputState) => ({
-                                title: newTitle,
-                                amount: prevInputState.amount
-                            })) }} />
+                                setEnteredTitle(event.target.value) }} />
                     </div>
                     <div className="form-control">
                         <label htmlFor="amount">Amount</label>
                         <input
                             type="number"
                             id="amount"
-                            value={ inputState.amount }
+                            value={ enteredAmount }
                             onChange={ event => {
-                                const newAmount = event.target.value
-                                setInputState((prevInputState) => ({
-                                amount: newAmount,
-                                title: prevInputState.title
-                            })) }} />
+                                setEnteredAmount(event.target.value) }} />
                     </div>
                     <div className="ingredient-form__actions">
                         <button type="submit">Add Ingredient</button>
